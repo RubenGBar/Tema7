9e813e94cfaab694696dc3519bb266b693ca3567
RubenGBar <ruben.garcia@iesnervion.es>
2024-04-30 14:00:16 +0200

    Segundo ejercicio terminado

---
+++ src/boletin1/ejercicio2/Principal.java
@@ -0,0 +1,21 @@
+package boletin1.ejercicio2;
+
+import java.util.LinkedHashSet;
+import java.util.Random;
+
+public class Principal {
+	public static void main(String[] args) {
+		
+		LinkedHashSet<Integer> set1 = new LinkedHashSet<>();
+		int i = 0;
+		Random rand = new Random();
+		
+		while(set1.size() < 10) {
+			set1.add(rand.nextInt(0, 21));
+		}
+		
+		System.out.println("Conjunto Único:\n" + set1);
+		
+	}
+	
+}
