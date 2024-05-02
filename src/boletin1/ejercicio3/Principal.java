9a9af2a4aa720fe2242024f701643322ba1a1c67
RubenGBar <ruben.garcia@iesnervion.es>
2024-05-02 12:56:57 +0200

    ejercicio 3 terminado

--- src/boletin1/ejercicio3/Principal.java
+++ src/boletin1/ejercicio3/Principal.java
@@ -1,23 +1,39 @@
 package boletin1.ejercicio3;
 
 import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class Principal {
 	public static void main(String[] args) {
-		String texto = "En un agujero en el suelo, vivía un hobbit. "
-				+ "No un agujero húmedo, sucio, repugnante, con restos de gusanos y olor a fango, "
+		String texto = "En un agujero en el suelo, vivia un hobbit. "
+				+ "No un agujero humedo, sucio, repugnante, con restos de gusanos y olor a fango, "
 				+ "ni tampoco un agujero, seco, desnudo y arenoso, sin nada en que sentarse "
 				+ "o que comer: era un agujero-hobbit, y eso significa comodidad";
 		
-		HashMap<String, Integer> mapa1 = new HashMap<>();
+		HashMap<Character, Integer> letras = new HashMap<>();
 		
 		String textoMinusculas = texto.toLowerCase();
 		
+		Character caracter;
+		
+		Set<Entry<Character, Integer>> caracteres = letras.entrySet();
+		
 		for(int i = 0; i < textoMinusculas.length(); i++) {
 			
-			if(!mapa1.containsKey(textoMinusculas.charAt(i))) {
-				mapa1.put(textoMinusculas.charAt(i) + "", null);
+			caracter = textoMinusculas.charAt(i);
+			
+			if(Character.isLetter(caracter)) {
+				if(letras.containsKey(caracter)) {
+					letras.put(caracter, letras.get(caracter)+1);
+				} else {
+					letras.put(caracter, 1);
+				}
 			}
+		}
+		
+		for(Entry<Character, Integer> entrada : caracteres) {
+			System.out.println(entrada.getKey() + " -> " + entrada.getValue());
 		}
 		
 	}
